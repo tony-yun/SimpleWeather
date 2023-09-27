4b28a59f29273a1ca674198d6dca2f322753986a
tony-yun <lifeyun24@naver.com>
2023-09-27 17:57:22 +0900

    active button to change weather label and image

--- SimpleWeather/WeatherViewController.swift
+++ SimpleWeather/WeatherViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    let cities = ["Seoul","Tokyo","LA","Seattle"]
+    let weathers = ["cloud.fill","sun.max.fill","wind","cloud.sun.rain.fill"]
+    
+    @IBAction func changeButtonTapped(_ sender: Any) {
+        
+        cityLabel.text = cities.randomElement()
+        weatherImageView.image = UIImage(systemName: weathers.randomElement()!)?.withRenderingMode(.alwaysOriginal)
+            // .withRenderingMode(.alwaysOriginal) 는 render mode = multicolor 이다.
+        
+        let randomTemp = Int.random(in: 10..<30)
+        temperatureLabel.text = "\(randomTemp)°"
     }
-    */
-
+    
+    
 }
