7cd487a541f1152e8bc3d35559f21c8118562fe1
tony-yun <lifeyun24@naver.com>
2023-09-27 17:30:16 +0900

    view layout settled.

---
+++ SimpleWeather/WeatherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherViewController.swift
+//  SimpleWeather
+//
+//  Created by 윤태웅 on 2023/09/26.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
